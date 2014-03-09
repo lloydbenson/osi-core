f99dce2923be565b777693078ea9d94d7b97d7a7
lloyd <lloyd@opensourceinfrastructure.com>
2014-03-09 15:45:44 -0500

    add in host profile

--- doc/curl.sh
+++ doc/curl.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl -X POST -H 'Content-Type: application/json' -d @host.json http://localhost:8080/api/host
+curl -X POST -H 'Content-Type: application/json' -d @profile/host.json http://localhost:8080/api/profile/host

417418cebafb34561afab3ac0cb9885abc1ed220
lloyd <lloyd@opensourceinfrastructure.com>
2014-03-15 07:29:04 -0500

    blah

--- doc/curl.sh
+++ doc/curl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "Adding a new host"
 curl -X POST -H 'Content-Type: application/json' -d @host.json http://localhost:8080/api/host
+echo "Adding a host profile"
 curl -X POST -H 'Content-Type: application/json' -d @profile/host.json http://localhost:8080/api/profile/host
+echo "Adding a network profile"
+curl -X POST -H 'Content-Type: application/json' -d @profile/network.json http://localhost:8080/api/profile/network
